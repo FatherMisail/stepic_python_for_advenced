b9b375766289481f8c50745dc468f077a0bc9e37
= <=>
2025-06-06 10:05:07 +0700

    Диагональная матрица: удалены лишние переменные

--- filling_diagonals.py
+++ filling_diagonals.py
@@ -1,12 +1,10 @@
-# n, m = 32, 12
+# n, m = 3, 5
 n, m = [int(i) for i in input().split()]
 M = [[0 for _ in range(m)] for _ in range(n)]
 
 frame = 0
 i_fr_shift = 0
 j_fr_shift = 0
-i = 0
-j = 0
 k = 1
 while k <= m*n:
     if frame + i_fr_shift < n and frame + j_fr_shift < m:
